1df05f2b8f7c343baaab3d4b86f5ffc567dff9e5
PrZ <prz@prznix.home>
2017-03-23 19:46:15 +0000

    Make build_usb parameter

--- build_usb.sh
+++ build_usb.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# XXX HARDCODED for now
-DEV="/dev/sdd"
+DEV=$1
+
+if [ -z $DEV ]; then
+	echo "please specify usb device"
+	exit 1
+fi
 
 
 sudo mount -o loop tmp-build-dir/remastered.iso /tmp/iso
-sudo mkfs.vfat /dev/sdd1 
-sudo mount /dev/sdd1 /tmp/flash
+sudo mkfs.vfat ${DEV}1
+sudo mount ${DEV}1 /tmp/flash
 sudo cp -a /tmp/iso/* /tmp/flash/ 2>&1 | egrep -v "Operation not permitted"
 sudo mv /tmp/flash/boot/isolinux/* /tmp/flash
 sudo mv /tmp/flash/isolinux.cfg /tmp/flash/syslinux.cfg
 sudo sed -i 's/APPEND \(.*\)/APPEND \1 cde=sda1\/cde/' /tmp/flash/syslinux.cfg
 sudo cp -a configs/syslinux/*.c32 /tmp/flash/
 sudo umount /tmp/flash
-sudo umount /tmp/sdd1
-sudo ms-sys -s /dev/sdd
-sudo syslinux /dev/sdd1
+sudo umount /tmp/iso
+sudo ms-sys -s ${DEV}
+sudo syslinux ${DEV}1
